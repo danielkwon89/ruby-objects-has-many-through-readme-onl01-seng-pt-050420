db8ebe5682754131541ff8a9fc561e54a66c44c1
Daniel Kwon <danielkwon89@gmail.com>
2020-05-29 21:04:00 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -23,6 +23,6 @@ class Waiter
   
   def best_tipper
     tip_array = Meal.all.map{|meal| meal.tip}
-    Meal.all[tip_array.find_index(tip_array.max)]
+    Meal.all.waiter[tip_array.find_index(tip_array.max)]
   end
 end
